3caf4f3dcfefcb71dbcc29d014f7d4ed65d06a83
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2022-11-23 14:16:33 +0800

    Update routes.py

--- backend/main/routes.py
+++ backend/main/routes.py
@@ -14,8 +14,6 @@ GOOGLE_CLIENT_ID=os.environ['GOOGLE_CLIENT_ID']
 GOOGLE_CLIENT_SECRET=os.environ['SECRET']
 
 client = WebApplicationClient(GOOGLE_CLIENT_ID)
-
-
 available_emails=['timothychenpc@gmail.com','tim20060112@gmail.com']
 
 
