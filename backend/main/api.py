f3a1bbd9909be7ac70e3ecb87df9bd0b3708defb
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2024-06-24 18:33:47 +0800

    version

--- backend/main/api.py
+++ backend/main/api.py
@@ -200,7 +200,7 @@ class staff(Resource):
                 send_notification(ic(msg_gen(data,dtype+'成功',args['time'])),mode=os.environ['MODE'])
                 
             db_model.collection.update_one({args['key']:args['value']},{'$set':{'log':log,'work':work,'workover':workover}})
-            return 'OK'
+            return 'OK',""
         else:
             print(colored("Card Not Found!",'red'))
             return 'Not Found'
