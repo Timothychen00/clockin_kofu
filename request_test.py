04dc396a1c7253563fa6f3666154a6f1edc0c5ae
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2024-08-03 18:28:53 +0800

    重複打卡時間的修正

--- request_test.py
+++ request_test.py
@@ -1,7 +1,7 @@
 import requests
 from icecream import ic
-host='https://friedclockin.azurewebsites.net'
+host='http://localhost:8000'
 path='/api/staff'
 headers={'Content-Type':'application/x-www-form-urlencoded'}
-response=requests.post(host+path,headers=headers,data={'connection_mode':'buttonless','key':'cardid','value':'136ca72'})
+response=requests.post(host+path,headers=headers,data={'connection_mode':'buttonless','key':'cardid','value':'87eae865'})
 ic( response.text)
