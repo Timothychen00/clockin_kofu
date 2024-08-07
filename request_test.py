1b79efd3c44cdd38f57bf81821d4584ffaea6e53
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2024-08-07 12:39:57 +0800

    手機顯示的bug

--- request_test.py
+++ request_test.py
@@ -1,7 +1,7 @@
 import requests
 from icecream import ic
-host='http://127.0.0.1:8000'
+host='https://bao7clockinsys.azurewebsites.net'
 path='/api/staff'
 headers={'Content-Type':'application/x-www-form-urlencoded'}
-response=requests.post(host+path,headers=headers,data={'connection_mode':'buttonless','key':'cardid','value':'cadkj'})
+response=requests.post(host+path,headers=headers,data={'connection_mode':'buttonless','key':'cardid','value':'87eae865'})
 ic( response.text)
